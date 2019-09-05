583f73d74c9947b8c04fc3519555241960ea5d53
Yuka Takahashi <yukatkh@gmail.com>
2019-09-05 11:56:59 -0700

    Fix unsharp mask to correct output

--- apps/scheduling-tool/test/unsharp_mask.cpp
+++ apps/scheduling-tool/test/unsharp_mask.cpp
@@ -16,51 +16,52 @@ int main(int argc, char **argv) {
     MachineParams params(32, 16000000, 40);
     Target target("x86-64-linux-sse41-avx-avx2");
 
-    Halide::Buffer<uint8_t> in = Halide::Tools::load_image("/home/yuka/Halide/apps/scheduling-tool/test/images/input.png");
+    Halide::Buffer<uint8_t> input = Halide::Tools::load_image("/home/yuka/Halide/apps/scheduling-tool/test/images/input.png");
 
     // Define a 7x7 Gaussian Blur with a repeat-edge boundary condition.
     float sigma = 1.5f;
 
     Var x, y, c;
-    Func kernel;
+    Func kernel("kernel");
     kernel(x) = exp(-x*x/(2*sigma*sigma)) / (sqrtf(2*M_PI)*sigma);
 
-    Func in_bounded = BoundaryConditions::repeat_edge(in);
+    Func in_bounded = BoundaryConditions::repeat_edge(input);
 
-    Func gray;
-    gray(x, y) = max(in_bounded(x, y, 0),
-                     max(in_bounded(x, y, 1),
-                         in_bounded(x, y, 2)));
+    Func gray("gray");
+    gray(x, y) = 0.299f * in_bounded(x, y, 0) + 0.587f * in_bounded(x, y, 1) +
+                 0.114f * in_bounded(x, y, 2);
 
-    Func blur_y;
+    Func blur_y("blur_y");
     blur_y(x, y) = (kernel(0) * gray(x, y) +
-                    kernel(1) * (gray(x, y-1) +
-                                 gray(x, y+1)) +
-                    kernel(2) * (gray(x, y-2) +
-                                 gray(x, y+2)) +
-                    kernel(3) * (gray(x, y-3) +
-                                 gray(x, y+3)));
+                    kernel(1) * (gray(x, y-1) + gray(x, y+1)) +
+                    kernel(2) * (gray(x, y-2) + gray(x, y+2)) +
+                    kernel(3) * (gray(x, y-3) + gray(x, y+3)));
 
-    Func blur_x;
+    Func blur_x("blur_x");
     blur_x(x, y) = (kernel(0) * blur_y(x, y) +
-                    kernel(1) * (blur_y(x-1, y) +
-                                 blur_y(x+1, y)) +
-                    kernel(2) * (blur_y(x-2, y) +
-                                 blur_y(x+2, y)) +
-                    kernel(3) * (blur_y(x-3, y) +
-                                 blur_y(x+3, y)));
+                    kernel(1) * (blur_y(x-1, y) + blur_y(x+1, y)) +
+                    kernel(2) * (blur_y(x-2, y) + blur_y(x+2, y)) +
+                    kernel(3) * (blur_y(x-3, y) + blur_y(x+3, y)));
 
-    Func sharpen;
+    Func sharpen("sharpen");
     sharpen(x, y) = 2 * gray(x, y) - blur_x(x, y);
 
-    Func ratio;
+    Func ratio("ratio");
     ratio(x, y) = sharpen(x, y) / gray(x, y);
 
-    Func result;
-    result(x, y, c) = cast<float>(ratio(x, y) * in(x, y, c));
+    Func unsharp("unsharp");
+    unsharp(x, y, c) = ratio(x, y) * input(x, y, c);
 
-    result.set_estimate(x, 0, in.width()).set_estimate(y, 0, in.height()).set_estimate(c, 0, in.channels());
-    Pipeline(result).auto_schedule(target, params);
+    unsharp.set_estimate(x, 0, input.width()).set_estimate(y, 0, input.height()).set_estimate(c, 0, input.channels());
+    Pipeline(unsharp).auto_schedule(target, params);
+
+    /*
+    Func output("output");
+    output(x, y, c)  = cast<uint8_t>(unsharp(x, y, c));
+    Buffer<uint8_t> buf(input.width(), input.height(), input.channels());
+    output.realize(buf);
+    Halide::Tools::save_image(buf, "neko.png");
+    */
 
     return 0;
 }
